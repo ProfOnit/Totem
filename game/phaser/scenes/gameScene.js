6c399dcbbae689e41859af937a4f5cf52430c22b
Halgand Ervin <halgand.ervin@gmail.com>
2020-02-01 17:17:41 +0100

    load map with csv map

--- game/phaser/scenes/gameScene.js
+++ game/phaser/scenes/gameScene.js
@@ -2,7 +2,8 @@ export class gameScene extends Phaser.Scene {
     constructor() {
         super({
             key: "Game",
-        })
+        });
+        this.secs = 60;
     }
 
     init() {
@@ -10,37 +11,60 @@ export class gameScene extends Phaser.Scene {
     }
 
     movementManager() {
-        this.dude.setVelocity(0, 0)
+        let x = this.dude.x;
+        let y = this.dude.y;
         if (this.keyboard.Z.isDown)
-            this.dude.setVelocity(0, -50)
+            y -= 16;
         if (this.keyboard.S.isDown)
-            this.dude.setVelocity(0, 50)
+            y += 16;
         if (this.keyboard.Q.isDown)
-            this.dude.setVelocity(-50, 0)
+            x -= 16;
         if (this.keyboard.D.isDown)
-            this.dude.setVelocity(50, 0)
-        if (this.keyboard.D.isDown && this.keyboard.S.isDown)
-            this.dude.setVelocity(50, 50)
-        if (this.keyboard.D.isDown && this.keyboard.Z.isDown)
-            this.dude.setVelocity(50, -50)
-        if (this.keyboard.Q.isDown && this.keyboard.S.isDown)
-            this.dude.setVelocity(-50, 50)
-        if (this.keyboard.Z.isDown && this.keyboard.Q.isDown)
-            this.dude.setVelocity(-50, -50)
+        x += 16;
+        this.dude.x = x;
+        this.dude.y = y;
     }
 
     preload() {
-        this.load.image("grass", "./assets/grass.jfif");
+
+        this.load.image("tile", "./assets/tile.png");
+
         this.load.image("dude", "./assets/dude.png");
+
+        this.load.tilemapCSV("map", "./assets/mapFin.csv");
     }
 
     create() {
+        
+
+        var map = this.make.tilemap({
+            key: "map",
+            tileWidth: 16,
+            tileHeight: 16
+        });
+        var tileset = map.addTilesetImage("tile");
+        var layer = map.createStaticLayer(0, tileset, 0, 0);
+        map.setLayer(layer)
+
         this.keyboard = this.input.keyboard.addKeys("Z, Q, S, D");
-        this.add.image(0, 0, "grass").setOrigin(0);
         this.dude = this.physics.add.image(300, 100, "dude");
+
+    }
+
+    tick() {
+        this.secs--;
+        if (this.secs == 0) {
+           this.movementManager();
+           this.secs = 60;
+        }
     }
 
     update() {
-        this.movementManager();
+        var timer = this.time.addEvent({
+            delay: 1000,
+            callback: this.tick,
+            callbackScope: this,
+            loop: true
+        });
     }
 }
